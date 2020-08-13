95e2af51e4d24f334703fbf7d32b1bd725a9369a
CJ Fulton <fulchr3356@gmail.com>
2020-08-13 14:59:15 -0400

    Completed assignment

--- src/main/java/com/github/perscholas/CartSystem.java
+++ src/main/java/com/github/perscholas/CartSystem.java
@@ -1,12 +1,23 @@
 package com.github.perscholas;
 
 
+import com.github.perscholas.utils.IOConsole;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CartSystem extends TheSystem {
-	CartSystem() {
+	IOConsole console = new IOConsole();
+	CartSystem() throws IOException {
+		super();
 	}
 
 	@Override
 	public void display() {
-		// Your code here
+		getItemCollection().forEach((s, item) -> console.print(s + " price: " + item.getItemPrice()) );
+		double sum = getItemCollection().values().stream().mapToDouble(item -> item.getQuantity() * item.getItemPrice()).sum();
+		System.out.println("Sum: " + sum);
 	}
+
 }
--- src/main/java/com/github/perscholas/MainApplication.java
+++ src/main/java/com/github/perscholas/MainApplication.java
@@ -1,8 +1,13 @@
 package com.github.perscholas;
 
+import java.util.StringJoiner;
+import java.util.stream.Stream;
+
 public class MainApplication {
     public static void main(String[] args) {
         Runnable myObject = new MyObject();
         myObject.run();
+
+
     }
 }
